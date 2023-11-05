2de559ecba2f061fc64657dcbea68660bbd095f6
Shyam Bisht <bishtshyam135@gmail.com>
2023-11-05 04:42:23 +0000

    added webapp image, added motion framer

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,17 +1,23 @@
 import home_img from "../../assets/home_page_logo.jpeg";
 import NavBar from "../Navigationbar/NavBar";
 import "./Home.css";
+import { motion } from "framer-motion";
 
 function Home() {
   return (
     <section id="home">
       <div className="home__page">
         <h1 className="my-name">Shyam Bisht</h1>
-        <img
-          className="home_img"
-          src={home_img}
-          alt="Park Glenveagh National Park"
-        />
+        <motion.div
+          whileHover={{scale: 1.05}}
+          whileTap={{scale: 0.9}}
+        >
+          <img
+            className="home_img"
+            src={home_img}
+            alt="Park Glenveagh National Park"
+          />
+        </motion.div>
         <div className="nav__bar">
           <NavBar />
         </div>
